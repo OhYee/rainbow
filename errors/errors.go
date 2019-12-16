b29c73c1cc833a849e996bf93fd77781b7b36188
OhYee <oyohyee@oyohyee.com>
2019-12-16 21:16:38 +0800

    misc: error struct return error type(#master)

--- errors/errors.go
+++ errors/errors.go
@@ -6,13 +6,14 @@ import (
 	"strings"
 )
 
+// Error struct
 type Error struct {
 	msg string
 }
 
-func New(format string, args ...interface{}) *Error {
+func getStack() string {
 	pcs := make([]uintptr, 100)
-	count := runtime.Callers(2, pcs)
+	count := runtime.Callers(3, pcs)
 	frames := runtime.CallersFrames(pcs)
 
 	s := make([]string, count)
@@ -24,17 +25,36 @@ func New(format string, args ...interface{}) *Error {
 			break
 		}
 	}
+	return strings.Join(s, "\n")
+}
 
+// New error with call stack
+func New(format string, args ...interface{}) error {
 	err := &Error{
 		msg: fmt.Sprintf(
 			"%s\n%s",
 			fmt.Sprintf(format, args...),
-			strings.Join(s, "\n"),
+			getStack(),
 		),
 	}
 	return err
 }
 
+// NewErr new error with error stack from error
+func NewErr(err error) error {
+	if err != nil {
+		return &Error{
+			msg: fmt.Sprintf(
+				"%s\n%s",
+				err.Error(),
+				getStack(),
+			),
+		}
+	}
+	return nil
+}
+
+// Error return the error message
 func (err *Error) Error() string {
 	return err.msg
 }
--- errors/errors_test.go
+++ errors/errors_test.go
@@ -11,17 +11,34 @@ func ErrCause() (err error) {
 }
 
 func TestError(t *testing.T) {
-	err := ErrCause()
+	var err error
+
+	err = ErrCause()
 	_, file, _, _ := runtime.Caller(0)
 	wantErr := fmt.Sprintf(
-		"%s\n%s:10 github.com/OhYee/rainbow/errors.ErrCause\n%s:14 github.com/OhYee/rainbow/errors.TestError\n",
+		"%s\n%s:10 github.com/OhYee/rainbow/errors.ErrCause\n%s:16 github.com/OhYee/rainbow/errors.TestError\n",
 		"err",
 		file,
+		file,
+	)
+	wantErr2 := fmt.Sprintf(
+		"%s\n%s:39 github.com/OhYee/rainbow/errors.TestError\n",
+		"err",
 		file,
 	)
 
 	if err.Error()[:len(wantErr)] != wantErr {
 		t.Errorf("want err %s, but got %s\n", wantErr, err)
+	}
+
+	err = NewErr(nil)
+	if err != nil {
+		t.Errorf("nil error returns %v", err)
+	}
+
+	err = NewErr(fmt.Errorf("err"))
+	if err.Error()[:len(wantErr2)] != wantErr2 {
+		t.Errorf("want err %s, but got %s\n", wantErr2, err)
 	}
 
 }
