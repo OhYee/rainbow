574705b089afa46a132569f60096a88b1a32ec90
OhYee <oyohyee@oyohyee.com>
2020-02-14 17:20:21 +0800

    feat: add wrapper function for defer(#master)

--- errors/errors.go
+++ errors/errors.go
@@ -11,9 +11,13 @@ type Error struct {
 	msg string
 }
 
-func getStack() string {
+func getStack(_skip ...int) string {
 	pcs := make([]uintptr, 100)
-	count := runtime.Callers(3, pcs)
+	skip := 3
+	if len(_skip) != 0 {
+		skip += _skip[0]
+	}
+	count := runtime.Callers(skip, pcs)
 	frames := runtime.CallersFrames(pcs)
 
 	s := make([]string, count)
@@ -41,20 +45,37 @@ func New(format string, args ...interface{}) error {
 }
 
 // NewErr new error with error stack from error
-func NewErr(err error) error {
-	if err != nil {
+func NewErr(err error, skip ...int) error {
+	if !IsError(err) && err != nil {
 		return &Error{
 			msg: fmt.Sprintf(
 				"%s\n%s",
 				err.Error(),
-				getStack(),
+				getStack(skip...),
 			),
 		}
 	}
-	return nil
+	return err
 }
 
 // Error return the error message
 func (err *Error) Error() string {
 	return err.msg
+}
+
+// Wrapper wrap error with stack.
+// func foo() (err error) {
+//     defer errors.Wrapper(&err)
+//     ... some code
+// }
+func Wrapper(err *error) {
+	if !IsError(*err) && err != nil {
+		*err = NewErr(*err, 1)
+	}
+}
+
+// IsError error is Rainbow Error or not
+func IsError(err error) bool {
+	_, isRainbowError := (err).(*Error)
+	return isRainbowError
 }
--- errors/errors_test.go
+++ errors/errors_test.go
@@ -3,6 +3,7 @@ package errors
 import (
 	"fmt"
 	"runtime"
+	"strings"
 	"testing"
 )
 
@@ -13,32 +14,58 @@ func ErrCause() (err error) {
 func TestError(t *testing.T) {
 	var err error
 
-	err = ErrCause()
 	_, file, _, _ := runtime.Caller(0)
 	wantErr := fmt.Sprintf(
-		"%s\n%s:10 github.com/OhYee/rainbow/errors.ErrCause\n%s:16 github.com/OhYee/rainbow/errors.TestError\n",
+		"%s\n%s:11 github.com/OhYee/rainbow/errors.ErrCause\n%s:42 github.com/OhYee/rainbow/errors.TestError\n",
 		"err",
 		file,
 		file,
 	)
 	wantErr2 := fmt.Sprintf(
-		"%s\n%s:39 github.com/OhYee/rainbow/errors.TestError\n",
+		"%s\n%s:47 github.com/OhYee/rainbow/errors.TestError\n",
+		"err",
+		file,
+	)
+	wantErr3 := fmt.Sprintf(
+		"%s\n%s:66 github.com/OhYee/rainbow/errors.TestError\n",
 		"err",
 		file,
 	)
 
-	if err.Error()[:len(wantErr)] != wantErr {
-		t.Errorf("want err %s, but got %s\n", wantErr, err)
-	}
-
+	// nil error
 	err = NewErr(nil)
 	if err != nil {
 		t.Errorf("nil error returns %v", err)
 	}
 
+	// stack test
+	err = ErrCause()
+	if !strings.HasPrefix(err.Error(), wantErr) {
+		t.Errorf("want err %s, but got %s\n", wantErr, err)
+	}
+
 	err = NewErr(fmt.Errorf("err"))
-	if err.Error()[:len(wantErr2)] != wantErr2 {
+	if !strings.HasPrefix(err.Error(), wantErr2) {
 		t.Errorf("want err %s, but got %s\n", wantErr2, err)
+	}
+
+	// NewErr
+	err2 := NewErr(err)
+
+	if err != err2 {
+		t.Errorf("want err %s, but got %s\n", err, err2)
+	}
+
+	// Wrapper
+	err2 = err
+	Wrapper(&err2)
+	if err != err2 {
+		t.Errorf("want err %s, but got %s\n", err, err2)
+	}
+	err2 = fmt.Errorf("err")
+	Wrapper(&err2)
+	if !strings.HasPrefix(err2.Error(), wantErr3) {
+		t.Errorf("want err %s, but got %s\n", wantErr3, err2)
 	}
 
 }
